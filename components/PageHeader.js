4606b755f336e4535d27a8a7d708a7049c3c8da8
Mykola Ratushniak <niiccoollaass442@gmail.com>
2024-04-11 00:42:03 +0300

    Fix problem with global components' vars

--- components/PageHeader.js
+++ components/PageHeader.js
@@ -1,97 +1,96 @@
-const template = document.createElement('template')
-
-template.innerHTML = `
-<style>
-:host {
-    display: block;
-    padding: 20px 0;
-}
-
-header {
-    display: flex;
-    flex-direction: row;
-    flex-wrap: wrap;
-    gap: 30px;
-    justify-content: space-between;
-    align-items: center;
-}
-
-::slotted([slot="title"]) {
-    display: block;
-    font-weight: 400;
-    font-size: 40px;
-    text-align: left;
-}
-
-nav {
-    display: flex;
-    flex-direction: row;
-    justify-content: end;
-    gap: 10px;
-}
-
-::slotted([slot="nav-link"]) {
-    width: 150px;
-    text-align: center;
-    font-size: 20px;
-    border-radius: 7px;
-    padding: 10px 30px;
-    font-weight: 300;
-    border: none;
-    opacity: 1;
-    background-color: var(--gray-dark);
-    color: var(--gray-light);
-    cursor: pointer;
-}
-
-::slotted([slot="nav-link"]:hover){
-    background-color: var(--gray);
-}
-
-::slotted([slot="nav-link"][data-active]){
-    background-color: var(--gray-light);
-}
-
-@media (width <= 700px) {
+class PageHeader extends HTMLElement {
+    template = () => `
+    <style>
+    * {
+        box-sizing: border-box;
+        margin: 0;
+    }
+    
     :host {
-        padding: 30px 0;
+        display: block;
+        padding: 20px 0;
     }
     
     header {
-        flex-direction: column;
+        display: flex;
+        flex-direction: row;
+        flex-wrap: wrap;
+        gap: 30px;
+        justify-content: space-between;
+        align-items: center;
     }
-}
-</style>
-
-<header>
-    <slot name="title"></slot>
     
-    <nav>
-        <slot name="nav-link"></slot>
-    </nav>
-</header>`
-
-class PageHeader extends HTMLElement {
-    connectedCallback() {
-        const shadow = this.attachShadow({ mode: 'open' })
-        shadow.appendChild(template.content.cloneNode(true))
-
-        shadow.querySelector('nav').addEventListener('click',
-            (event) => this.updateActiveNavOption(event))
+    ::slotted([slot="title"]) {
+        display: block;
+        font-weight: 400;
+        font-size: 40px;
+        text-align: left;
     }
+    
+    nav {
+        display: flex;
+        flex-direction: row;
+        justify-content: end;
+        gap: 10px;
+    }
+    
+    ::slotted([slot="nav-link"]) {
+        width: 150px;
+        text-align: center; 
+        font-size: 20px;
+        border-radius: 7px;
+        padding: 10px 30px;
+        font-weight: 300;
+        border: none;
+        opacity: 1;
+        background-color: var(--gray-dark);
+        color: var(--gray-light);
+        cursor: pointer;
+    }
+    
+    ::slotted([slot="nav-link"]:hover){
+        background-color: var(--gray);
+    }
+    
+    ::slotted([slot="nav-link"][data-active]){
+        background-color: var(--gray-light);
+    }
+    
+    @media (width <= 700px) {
+        :host {
+            padding: 30px 0;
+        }
+    
+        header {
+            flex-direction: column;
+        }
+    }
+    </style>
+    
+    <header>
+        <slot name="title"></slot>
+        <nav>
+            <slot name="nav-link"></slot>
+        </nav>
+    </header>`
 
-    updateActiveNavOption(event) {
+    render = () => this.shadowRoot.innerHTML = this.template().trim()
+
+    updateActiveNavOption = (event) => {
+        console.log(this.shadowRoot)
         const target = event.target
 
         if (target.getAttribute('slot') !== 'nav-link') {
             return
-        }else if (target.hasAttribute('data-active')) {
+        } else if (target.hasAttribute('data-active')) {
             event.preventDefault()
             return;
         }
 
         event.preventDefault()
-        this.shadowRoot.host.querySelectorAll('[slot="nav-link"]')
+
+        this.shadowRoot.host
+            .querySelectorAll('[slot="nav-link"]')
             .forEach(el => delete el.dataset.active)
 
         target.setAttribute('data-active', '')
@@ -101,6 +100,21 @@ class PageHeader extends HTMLElement {
             cancelable: true,
             detail: {route: target.href}
         }))
+    }
+
+    connectedCallback() {
+        this.attachShadow({mode: 'open'})
+
+        if (this.rendered) {
+            return
+        }
+
+        this.render()
+        this.rendered = true
+
+        this.shadowRoot
+            .querySelector('nav')
+            .addEventListener('click', this.updateActiveNavOption)
     }
 }
 
